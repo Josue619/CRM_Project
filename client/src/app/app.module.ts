9a15d0e6c02bf00aac2e7b4f6bfd67b8ecbc3a85
Josué Martínez <josue.martinez.cz@gmail.com>
2020-05-18 12:46:25 -0600

    The frontend structure and components are created for user authentication

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/src/app/models/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id?: number;
+    username?: string;
+    email?: string;
+    password?: string;
+    card_id?: number;
+    phone?: number;
+    roll?: string;
+    state?: boolean;
+    created_at?: Date;
+}
